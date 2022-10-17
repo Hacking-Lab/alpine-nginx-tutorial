9757d6e66bf4024193c1745ac23c49c7fd6c7e1c
Ivan Buetler <ivan.buetler@compass-security.com>
2022-10-17 11:47:30 +0200

    change to s6 overlay version 3

--- root/flag-deploy-scripts/deploy-file-flag.sh
+++ root/flag-deploy-scripts/deploy-file-flag.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/with-contenv bash
+#!/command/with-contenv bash
 
 echo "put your commands to deploy the file based flag here"
 echo "the /goldnugget/*.gn contains the flag"
 
 # take flag and store it where it is needed
 source /goldnugget/*.gn
+echo $GOLDNUGGET
 sed -i -e "s/SED_GOLDNUGGET/$GOLDNUGGET/g" /opt/www/Readme.md
 
-
-# remove flag from docker
-cat /dev/null > /goldnugget/*.gn
