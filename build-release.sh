b977647d6fc8115e5c575996e6edc87de0ca8d83
Ivan Buetler <ivan.buetler@compass-security.com>
2022-11-08 20:39:38 +0100

    fixing base to tutorial

--- build-release.sh
+++ build-release.sh
@@ -5,13 +5,13 @@ case `uname -m` in
 	x86_64)
 		echo "=============================================="
 		echo "building x86_64"
-		docker buildx build --push --platform linux/amd64 --no-cache -t hackinglab/alpine-base-tutorial:amd64-latest -t hackinglab/alpine-base-tutorial:amd64-$1 -t hackinglab/alpine-base-tutorial:amd64-$1.0 -f Dockerfile .
+		docker buildx build --push --platform linux/amd64 --no-cache -t hackinglab/alpine-nginx-tutorial:amd64-latest -t hackinglab/alpine-nginx-tutorial:amd64-$1 -t hackinglab/alpine-nginx-tutorial:amd64-$1.0 -f Dockerfile .
 	;;
 
 	aarch64)
                 echo "=============================================="
                 echo "building arm64"
-                docker buildx build --push --platform linux/arm64 --no-cache -t hackinglab/alpine-base-tutorial:arm64-latest -t hackinglab/alpine-base-tutorial:arm64-$1 -t hackinglab/alpine-base-tutorial:arm64-$1.0 -f Dockerfile .
+                docker buildx build --push --platform linux/arm64 --no-cache -t hackinglab/alpine-nginx-tutorial:arm64-latest -t hackinglab/alpine-nginx-tutorial:arm64-$1 -t hackinglab/alpine-nginx-tutorial:arm64-$1.0 -f Dockerfile .
 	;;
 
 	*)
--- do_manifest.sh
+++ do_manifest.sh
@@ -1,6 +1,6 @@
-docker manifest create hackinglab/alpine-base-tutorial --amend hackinglab/alpine-base-tutorial:amd64-latest --amend hackinglab/alpine-base-tutorial:arm64-latest
-docker manifest create hackinglab/alpine-base-tutorial:3.2 --amend hackinglab/alpine-base-tutorial:amd64-3.2 --amend hackinglab/alpine-base-tutorial:arm64-3.2
-docker manifest create hackinglab/alpine-base-tutorial:3.2.0 --amend hackinglab/alpine-base-tutorial:amd64-3.2.0 --amend hackinglab/alpine-base-tutorial:arm64-3.2.0
-docker manifest push hackinglab/alpine-base-tutorial
-docker manifest push hackinglab/alpine-base-tutorial:3.2
-docker manifest push hackinglab/alpine-base-tutorial:3.2.0
+docker manifest create hackinglab/alpine-nginx-tutorial --amend hackinglab/alpine-nginx-tutorial:amd64-latest --amend hackinglab/alpine-nginx-tutorial:arm64-latest
+docker manifest create hackinglab/alpine-nginx-tutorial:3.2 --amend hackinglab/alpine-nginx-tutorial:amd64-3.2 --amend hackinglab/alpine-nginx-tutorial:arm64-3.2
+docker manifest create hackinglab/alpine-nginx-tutorial:3.2.0 --amend hackinglab/alpine-nginx-tutorial:amd64-3.2.0 --amend hackinglab/alpine-nginx-tutorial:arm64-3.2.0
+docker manifest push hackinglab/alpine-nginx-tutorial
+docker manifest push hackinglab/alpine-nginx-tutorial:3.2
+docker manifest push hackinglab/alpine-nginx-tutorial:3.2.0
