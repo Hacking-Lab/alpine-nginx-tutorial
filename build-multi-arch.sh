1a728d27c6347273e6a06a64653f5ab25b0fae18
ibuetler <ivan.buetler@hacking-lab.com>
2024-11-11 22:50:00 +0100

    fixing target image

--- build-multi-arch.sh
+++ build-multi-arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:latest . --push
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:$1  . --push
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-base-hl:$1.0 . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-nginx-tutorial:latest . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-nginx-tutorial:$1  . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-nginx-tutorial:$1.0 . --push
 
